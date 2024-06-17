e733d4f7285493db0d3110f60bd6beaa36028eeb
Marco <MARCO.GARBANZO@ucr.ac.cr>
2024-06-16 21:11:11 -0600

    Adding Class

---
+++ Proyecto_EMUS/Models/Pacient.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_EMUS.Models
+{
+    public class Pacient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DayOfBirth { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FistName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        public string CivilStatus { get; set; }
+
+
+
+        
+    }
+}
