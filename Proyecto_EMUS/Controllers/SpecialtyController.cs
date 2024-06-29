9cd55cf50f5c6603c9eefe117e3d3a9bd0af7b56
Marco <MARCO.GARBANZO@ucr.ac.cr>
2024-06-29 17:49:16 -0600

    testing installing NuGet-packages

---
+++ Proyecto_EMUS/Controllers/SpecialtyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_EMUS.Controllers
+{
+    public class SpecialtyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
