371983b0b7f5f89f6e422977f4c5016968aa4592
Gabriel <gabriel.carranzarodriguez@ucr.ac.cr>
2024-06-29 20:56:02 -0600

    UnitOfWork added in Program.cs

--- Proyecto_EMUS/Program.cs
+++ Proyecto_EMUS/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Proyecto_EMUS.Data;
+using Proyecto_EMUS.Data.Repository.Interfaces;
+using Proyecto_EMUS.Data.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
