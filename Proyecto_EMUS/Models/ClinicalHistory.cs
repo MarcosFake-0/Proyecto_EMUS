cc14b9fac7b695e17ecfc579e1733d7fd90f703f
Ashley Valverde <ashley.torresvalverde@ucr.ac.cr>
2024-07-01 22:19:42 -0600

    Clinical History Models

---
+++ Proyecto_EMUS/Models/ClinicalHistory.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_EMUS.Models
+{
+    public class ClinicalHistory
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ Proyecto_EMUS/Models/ClinicalHistoryNote.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto_EMUS.Models
+{
+    public class ClinicalHistoryNote
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
