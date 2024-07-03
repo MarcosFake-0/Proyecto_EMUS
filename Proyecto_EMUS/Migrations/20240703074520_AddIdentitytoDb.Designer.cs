c3ca3a09544836f82c78d0ac475157dc3162143c
Marco <MARCO.GARBANZO@ucr.ac.cr>
2024-07-03 01:46:40 -0600

    - adding migrations - updating db

--- Proyecto_EMUS/Migrations/20240703074520_AddIdentitytoDb.Designer.cs
+++ Proyecto_EMUS/Migrations/20240703074520_AddIdentitytoDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proyecto_EMUS.Data;
 
@@ -11,9 +12,11 @@ using Proyecto_EMUS.Data;
 namespace Proyecto_EMUS.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240703074520_AddIdentitytoDb")]
+    partial class AddIdentitytoDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
