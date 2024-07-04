0de3b47f2456437b90817096723cf75bce2e5d9c
Gabriel <gabriel.carranzarodriguez@ucr.ac.cr>
2024-07-03 19:03:47 -0600

    Medication CRUD finished. Medication migration fixed

---
+++ Proyecto_EMUS/Data/Migra/20240704005948_FixingMedication.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_EMUS.Data.Migra
+{
+    /// <inheritdoc />
+    public partial class FixingMedication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Medications",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Proyecto_EMUS/wwwroot/js/medication.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        ajax: {
+            "url": "/Medicine/Medication/getall"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <a href="/Medicine/Medication/upsert/${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+
+                            <a onClick=Delete(${data}) class="btn btn-danger mx-2">
+                                <i class="bi bi-trash"></i> Delete
+                            </a>
+                          `
+                }
+            }
+        ]
+    });
+}
+
+function Delete(_id) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            $.ajax({
+                url: "/Medicine/Medication/delete/" + _id,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                },
+                error: function () {
+                    toastr.error("Error connecting to endpoint");
+                }
+            });
+        }
+    });
+}
